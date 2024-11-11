c88af5eec2c8a8d669a2a177297588006ddd9a2c
SpencerCragun <spencer.cragun@gmail.com>
2024-11-11 09:40:49 -0800

    connect api, render books in list

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,31 @@ import { useState, useEffect } from 'react'
 
 function App() {
 
+const [books, setBooks] = useState([]);
+
+useEffect(() => {
+  const fetchBooks = async () => {
+    const data = await fetch (`https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/books`)
+    const allData = await data.json();
+    setBooks(allData.books)
+  }
+
+  fetchBooks();
+
+}, [])
+
+
   return (
     <>
-      
+      <h1>Book Buddy!</h1>
+
+
+      <ul>{books.map((book) => {
+        console.log(book);
+        return <li key={book.id}>{book.title}
+        <img src={book.coverimage} style={{width: 200, height: 350,}}></img></li> 
+      })}
+      </ul>
     </>
   )
 }
